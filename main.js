89b378c1eb2ad19874c11f4674742c8842aacd11
minh040508 <minhvux074@gmail.com>
2025-02-09 23:16:31 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Thay đổi nội dung búc thư ở đây
-var letterContent =" Cảm ơn em đã dành thời gian để nhìn những trò trẻ con của anhh và anh có những điều muốn gởi gắm đến tình iu của anhh❤️. Cảm ơn em đã đến bên anhh những lúc anh cô đơn buồn tủi nhất.Cuộc sống thực sự chẳng có ý nghĩa gì nếu không có em. Em làm cho anh luôn cảm thấy đặc biệt và hoàn hảo. Anh chẳng biết nói gì ngoài lời cảm ơn em, anh rất yêu em💕"
+var letterContent ="Chúc ebeee valentine vui vẻ. Đây là ngày lễ tình nhân đầu tiên của chúng ta bên nhau, tôi rất biết ơn khi có bạn bên cạnh. Đây là nhiều năm nữa của tình yêu, hạnh phúc và tạo ra nhiều khoảnh khắc khó quên bên nhau! Tôi yêu bạn nhiều hơn lời nói có thể diễn tả. Yêu bạn💕"
 
 // Tốc độ viết chữ. Số càng nhỏ tốc độ càng nhanh. 50 là tốc độ khá phù hợp
 durationWrite = 50 
